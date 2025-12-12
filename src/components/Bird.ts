65b35f72a129604663b026d3c23201c912bcd09a
Juan Peruzzo <juanperuzzo13@gmail.com>
2025-12-12 08:27:50 -0300

    feat: add Bird interface and implement initial game loop with gravity

---
+++ src/components/Bird.ts
@@ -0,0 +1,7 @@
+export interface Bird {
+    x: number;
+    y: number;
+    width: number;
+    height: number;
+    velocity: number;
+}
--- src/components/GameCanvas.tsx
+++ src/components/GameCanvas.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useRef } from "react";
+import type { Bird } from "./Bird"
 
 export function GameCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
@@ -10,8 +11,30 @@ export function GameCanvas() {
     const ctx = canvas.getContext("2d");
     if (!ctx) return;
 
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    const bird: Bird  = {
+        x: 50,
+        y: canvas.height / 2,
+        width: 30,
+        height: 30,
+        velocity: 0,
+    };
+
+    const gravity = 0.4;
+
+    const update = () => {
+        bird.velocity += gravity;
+        bird.y += bird.velocity;
+
+        ctx.fillStyle = "#000";
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+        ctx.fillStyle = "yellow";
+        ctx.fillRect(bird.x, bird.y, bird.width, bird.height);
+        
+        requestAnimationFrame(update);
+    }
+
+    update();
   }, []);
 
   return (
