d6c6ba38c26dbcfd8fc79e46e197375005a75895
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-02-20 15:23:52 -0300

    feat: implement Vercel Analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,23 @@
 import { GameCanvas } from "./components/GameCanvas";
 import MainMenu from "./components/MainMenu";
 import { useState } from "react";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
   const [gameStarted, setGameStarted] = useState(false);
 
   if (!gameStarted) {
-    return <MainMenu onPlay={() => setGameStarted(true)} />;
+    return (
+      <>
+        <MainMenu onPlay={() => setGameStarted(true)} />
+        <Analytics />
+      </>
+    );
   }
   return (
     <div>
       <GameCanvas />
+      <Analytics />
     </div>
   );
 }
