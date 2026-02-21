51ffb387c00372d23745ba721c3dd37444239ee8
Juan Peruzzo <juanperuzzo13@gmail.com>
2026-02-20 22:12:23 -0300

    feat: add PWA support

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,6 +8,32 @@ export default defineConfig({
     react({
       babel: {
         plugins: [['babel-plugin-react-compiler']],
+      },
+    }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.png'],
+      manifest: {
+        name: 'Flappynaruu',
+        short_name: 'Flappynaruu',
+        description: 'Ajude Naruu a escapar da gaiola e realizar seu sonho!',
+        start_url: '/',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        icons: [
+          {
+            src: '/favicon.png',
+            sizes: '500x500',
+            type: 'image/png',
+          },
+          {
+            src: '/favicon.png',
+            sizes: '500x500',
+            type: 'image/png',
+            purpose: 'maskable',
+          },
+        ],
       },
     }),
   ],
